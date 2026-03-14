ccbb8eb3b3c9b2d6d9789bac98aba741ca3c7f91
Sourav-1026 <faruqueomar740@gmail.com>
2026-03-14 22:20:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h3>World on the go..</h3>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Countries = () => {
+  return (
+    <div>
+      <p>In the countries: </p>
+    </div>
+  );
+};
+
+export default Countries;
